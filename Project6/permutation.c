89415ba6a523c0d0a04045f0dea99905a9413c6e
Ju Yeong Yoo <yoojy1012@gmail.com>
2019-11-20 23:29:30 +0900

    2

--- Project6/permutation.c
+++ Project6/permutation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
 
@@ -19,4 +20,6 @@ int main(int argc, char* argv[]) {
 		fputs(str, fp2);
 	}
 
+	int nNumber = fgets(str, 10, fp1);
+	int* pChar = (int*)calloc(nNumber, sizeof(char));
 }
